63ca1349224e16b730c4e82187ee36e69cdd1e32
Mirosław Karczmarczyk <fnxnet@gmail.com>
2019-08-27 18:49:32 +0200

    adding getters and setters

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ include_once __DIR__ . '/vendor/autoload.php';
 use \Eduweb\Car;
 
 $car = new Car('Ford', 'Mondeo');
+$car->setSpeed(1.5);
 
 var_dump($car);
+var_dump('Speed:', $car->getSpeed() * 2);
 
 $car->drive();
--- src/Car.php
+++ src/Car.php
@@ -6,11 +6,26 @@ class Car
 {
     protected $make = '';
     protected $model = '';
+    protected $speed = 0.0;
 
-    public function __construct(string $make, string $model)
+    public function __construct(string $make, string $model, Speed $speed)
     {
         $this->make = $make;
         $this->model = $model;
+        $this->speed = $speed;
+    }
+
+    public function setSpeed(float $speed) : void
+    {
+        if ($speed < 0) {
+            return;
+        }
+
+        $this->speed = $speed;
+    }
+
+    public function getSpeed() : float{
+        return $this->speed;
     }
 
     public function drive() : void
