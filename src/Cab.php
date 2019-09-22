3706db7114905d09835987692956197a0e0f10c3
Mirosław Karczmarczyk <fnxnet@gmail.com>
2019-09-22 14:08:10 +0200

    adding private access rights

---
+++ src/Cab.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Eduweb;
+
+class Cab extends Car
+{
+
+    private $color = 'yellow';
+
+    public function drive() : void
+    {
+        echo 'Driving Cab';
+    }
+
+    public function getColor() : string
+    {
+        return $this->color;
+    }
+}
