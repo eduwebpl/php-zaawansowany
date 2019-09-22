cf4183a51f951014594993657a14f7f1ce4064e6
Mirosław Karczmarczyk <fnxnet@gmail.com>
2019-09-22 16:04:01 +0200

    starting game

--- index.php
+++ index.php
@@ -2,15 +2,7 @@
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-use \Eduweb\Car;
-use \Eduweb\Cab;
-use \Eduweb\Speed;
+use \Eduweb\Game\Race;
 
-$speed = new Speed(1.5);
-
-$cab = new Cab('Ford', 'Mondeo', $speed);
-
-$car = new Car('Ford', 'Mondeo', $speed);
-
-echo 'Car Color: ' . $car->getColor() . "\n";
-echo 'Cab Color: ' . $cab->getColor() . "\n";
+$race = new Race();
+$race->run();
