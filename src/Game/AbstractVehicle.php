33b677ae649f48ef229e59fe8892ac4379c3d45a
Mirosław Karczmarczyk <fnxnet@gmail.com>
2019-12-09 18:16:03 +0100

    adding abstract methods

--- src/Game/AbstractVehicle.php
+++ src/Game/AbstractVehicle.php
@@ -6,6 +6,7 @@ abstract class AbstractVehicle implements Vehicle
 {
     protected $name;
     protected $type;
+    protected $distance = 0;
 
     public function __construct(string $name)
     {
@@ -14,13 +15,21 @@ abstract class AbstractVehicle implements Vehicle
 
     public function move() : void
     {
-        echo "\n Moving {$this->getType()} ({$this->getName()})";
+        $this->preMove();
+
+        echo "\n Moving {$this->getType()} ({$this->getName()}) by {$this->distance}";
+
+        $this->postMove();
     }
 
     protected function getType() : string
     {
         return $this->type;
     }
+
+    protected function postMove() : void { }
+
+    protected function preMove() : void { }
 
     abstract protected function getName() : string;
 }
--- src/Game/Motor.php
+++ src/Game/Motor.php
@@ -5,9 +5,16 @@ namespace Eduweb\Game;
 class Motor extends AbstractVehicle
 {
     protected $type = 'Motor';
+    protected $maxSpeed = 250;
 
     protected function getName() : string
     {
         return $this->name;
     }
+
+    protected function preMove() : void
+    {
+        $this->distance += $this->maxSpeed * rand(20, 100) / 100;
+    }
+
 }
